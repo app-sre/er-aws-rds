3bc3a1a232f567941cb34eef5fc52e7fa046e1a0
Di Wang <dwan@redhat.com>
2025-02-18 18:08:24 +1100

    feat: handle blue green deployment not enabled
    Signed-off-by: Di Wang <dwan@redhat.com>

--- hooks/utils/blue_green_deployment_manager.py
+++ hooks/utils/blue_green_deployment_manager.py
@@ -1,3 +1,5 @@
+import logging
+
 from er_aws_rds.input import AppInterfaceInput
 from hooks.utils.aws_api import AWSApi
 
@@ -15,7 +17,13 @@ class BlueGreenDeploymentManager:
         self.aws_api = aws_api
         self.app_interface_input = app_interface_input
         self.dry_run = dry_run
+        self.logger = logging.getLogger(__name__)
 
     def run(self) -> None:
         """Manage Blue/Green Deployment"""
-        pass
+        blue_green_deployment = self.app_interface_input.data.blue_green_deployment
+        if blue_green_deployment is None or not blue_green_deployment.enabled:
+            self.logger.info(
+                "Blue/Green Deployment not enabled, continue to normal flow."
+            )
+            return
