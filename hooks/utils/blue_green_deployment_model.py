6792766407baac170c399e79bcef5af4ace8b5bf
Di Wang <dwan@redhat.com>
2025-04-10 20:02:02 +1000

    refactor: explict check backup_retention_period greater than 0
    Signed-off-by: Di Wang <dwan@redhat.com>

--- hooks/utils/blue_green_deployment_model.py
+++ hooks/utils/blue_green_deployment_model.py
@@ -114,7 +114,10 @@ class BlueGreenDeploymentModel(BaseModel):
     @model_validator(mode="after")
     def _validate_backup_retention_period(self) -> Self:
         if self.db_instance and self.db_instance["BackupRetentionPeriod"] <= 0:
-            if self.input_data.backup_retention_period:
+            if (
+                self.input_data.backup_retention_period
+                and self.input_data.backup_retention_period > 0
+            ):
                 self._pending_prepares.add(PendingPrepare.BACKUP_RETENTION_PERIOD)
             else:
                 raise ValueError("backup_retention_period must be greater than 0")
